3291761ea88a4cc28f909b67eaa57346c8a95247
eunjuch <juicy123123@naver.com>
2023-09-14 15:21:21 +0900

    new java file

---
+++ sample/src/sample/Sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Sample {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Your name: ");
+		String name = sc.next();
+		System.out.println(name + " Hello!");
+	}
+}
